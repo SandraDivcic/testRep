c76a977c1eb190db75bae91ba06e84ab7747088d
SandraDivcic <sandra.divcic@gmail.com>
2016-07-28 21:44:51 +0200

    Drugi commit

--- src/gitTest/GitHello.java
+++ src/gitTest/GitHello.java
@@ -2,9 +2,16 @@ package gitTest;
 
 public class GitHello {
 
+	public static int suma(int x, int y)
+	{
+		int z=x+y;
+		return z;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prvi git commit");
+		System.out.println("Treci git commit");
+		System.out.println(suma(1,2));
 	}
 
 }
